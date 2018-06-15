37f3182cd4f0465992eb06d7ed8f4fcf40cf3a67
thewart <seth.madlonkay@gmail.com>
2018-06-15 16:33:21 -0400

    continued cleanup

--- postcast.R
+++ postcast.R
@@ -1,18 +1,19 @@
 # merge all casted data tables
 merged_behaviors <- Reduce(merge,casts)
 
+# merge covariates
+merged_behaviors <-  merge(merged_behaviors,pedigree[,c(1, 2, 11)], by = "Focal_ID")
+merged_behaviors[,Age:=Year - CS.BIRTH.SEASON]
+merged_behaviors[,CS.BIRTH.SEASON:=NULL]
+merged_behaviors <- merge(merged_behaviors,dominance[,c("ID","YEAR","ORD_RANK")],by.x=c("Focal_ID","Year"),by.y=c("ID","YEAR"))
+merged_behaviors[,ORD_RANK:=ordered(ORD_RANK,levels=c("L","M","H"))]
+nc <- ncol(merged_behaviors)
+merged_behaviors <- merged_behaviors[,c(1,(nc-2):nc,2:(nc-3)),with=F]
+
 # merge genotype and behavioral data
-merged_OXTR_AVPR1 <- merge(merged_behaviors, all_genotypes, by = "Focal_ID")
+#merged_OXTR_AVPR1 <- merge(merged_behaviors, all_genotypes, by = "Focal_ID")
 # rm(merged_behaviors)
 
-# merge and calculate age data
-merged_OXTR_AVPR1 <-  merge(merged_OXTR_AVPR1,pedigree[,c(1, 2, 11)], by = "Focal_ID")
-merged_OXTR_AVPR1[,Age:=Year - CS.BIRTH.SEASON]
-merged_OXTR_AVPR1[,CS.BIRTH.SEASON:=NULL]
-merged_OXTR_AVPR1 <- merge(merged_OXTR_AVPR1,dominance[,c("ID","YEAR","ORD_RANK")],by.x=c("Focal_ID","Year"),by.y=c("ID","YEAR"))
-merged_OXTR_AVPR1[,ORD_RANK:=ordered(ORD_RANK,levels=c("L","M","H"))]
-nc <- ncol(merged_OXTR_AVPR1)
-merged_OXTR_AVPR1 <- merged_OXTR_AVPR1[,c(1,(nc-2):nc,2:(nc-3)),with=F]
 #merged_OXTR_AVPR1 <- merged_OXTR_AVPR1[,c(1:3, ncol(merged_OXTR_AVPR1), 4:(ncol(merged_OXTR_AVPR1))-1),with=F]
 
 # create new column that combines ID and year
